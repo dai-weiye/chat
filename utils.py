92a7ce33612029332e36060f3a1323e5e747a41b
dai-weiye <167695009+dai-weiye@users.noreply.github.com>
2024-10-10 23:21:56 +0800

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,8 @@
+from langchain.chains import ConversationChain
+from langchain_openai import ChatOpenAI
+def get_chat_response(prompt, memory, openai_api_key):
+    model = ChatOpenAI(model="gpt-4o", openai_api_key=openai_api_key)
+    chain = ConversationChain(llm=model, memory=memory)
+
+    response = chain.invoke({"input": prompt})
+    return response["response"]
